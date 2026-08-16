173fc71568e535977932c4fef694d504c8dfbcf2
shiv-1904 <82521927+shiv-1904@users.noreply.github.com>
2026-08-16 17:02:44 +0530

    Add files via upload

--- rgb2dvi/xgui/rgb2dvi_v1_2.tcl
+++ rgb2dvi/xgui/rgb2dvi_v1_2.tcl
@@ -1,26 +1,31 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
-  set Component_Name  [  ipgui::add_param $IPINST -name "Component_Name" -display_name {Component Name}]
-  set_property tooltip {Component Name} ${Component_Name}
+  ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
-  set Page_0  [  ipgui::add_page $IPINST -name "Page 0" -display_name {Page 0}]
-  set_property tooltip {Page 0} ${Page_0}
-  set kRstActiveHigh  [  ipgui::add_param $IPINST -name "kRstActiveHigh" -parent ${Page_0} -display_name {Reset active high}]
-  set_property tooltip {Reset active high} ${kRstActiveHigh}
-  set kGenerateSerialClk  [  ipgui::add_param $IPINST -name "kGenerateSerialClk" -parent ${Page_0} -display_name {Generate SerialClk internally from pixel clock.}]
-  set_property tooltip {Generate SerialClk internally from pixel clock.} ${kGenerateSerialClk}
-  set kClkPrimitive  [  ipgui::add_param $IPINST -name "kClkPrimitive" -parent ${Page_0} -display_name {MMCM/PLL}]
-  set_property tooltip {MMCM/PLL} ${kClkPrimitive}
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "kRstActiveHigh" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "kGenerateSerialClk" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "kClkPrimitive" -parent ${Page_0} -layout horizontal
+  ipgui::add_param $IPINST -name "kClkRange" -parent ${Page_0} -layout horizontal
 
 
 }
 
-proc update_PARAM_VALUE.kRstActiveHigh { PARAM_VALUE.kRstActiveHigh } {
-	# Procedure called to update kRstActiveHigh when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.TMDS_BOARD_INTERFACE { PARAM_VALUE.TMDS_BOARD_INTERFACE } {
+	# Procedure called to update TMDS_BOARD_INTERFACE when any of the dependent parameters in the arguments change
 }
 
-proc validate_PARAM_VALUE.kRstActiveHigh { PARAM_VALUE.kRstActiveHigh } {
-	# Procedure called to validate kRstActiveHigh
+proc validate_PARAM_VALUE.TMDS_BOARD_INTERFACE { PARAM_VALUE.TMDS_BOARD_INTERFACE } {
+	# Procedure called to validate TMDS_BOARD_INTERFACE
+	return true
+}
+
+proc update_PARAM_VALUE.kClkRange { PARAM_VALUE.kClkRange } {
+	# Procedure called to update kClkRange when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.kClkRange { PARAM_VALUE.kClkRange } {
+	# Procedure called to validate kClkRange
 	return true
 }
 
@@ -30,6 +35,15 @@ proc update_PARAM_VALUE.kClkPrimitive { PARAM_VALUE.kClkPrimitive } {
 
 proc validate_PARAM_VALUE.kClkPrimitive { PARAM_VALUE.kClkPrimitive } {
 	# Procedure called to validate kClkPrimitive
+	return true
+}
+
+proc update_PARAM_VALUE.kRstActiveHigh { PARAM_VALUE.kRstActiveHigh } {
+	# Procedure called to update kRstActiveHigh when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.kRstActiveHigh { PARAM_VALUE.kRstActiveHigh } {
+	# Procedure called to validate kRstActiveHigh
 	return true
 }
 
@@ -56,5 +70,10 @@ proc update_MODELPARAM_VALUE.kClkPrimitive { MODELPARAM_VALUE.kClkPrimitive PARA
 proc update_MODELPARAM_VALUE.kRstActiveHigh { MODELPARAM_VALUE.kRstActiveHigh PARAM_VALUE.kRstActiveHigh } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.kRstActiveHigh}] ${MODELPARAM_VALUE.kRstActiveHigh}
+}
+
+proc update_MODELPARAM_VALUE.kClkRange { MODELPARAM_VALUE.kClkRange PARAM_VALUE.kClkRange } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.kClkRange}] ${MODELPARAM_VALUE.kClkRange}
 }
 
